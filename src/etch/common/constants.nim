cf8156a9a8a00a7eddd7958cdb13ddaa85deac5a
kunitoki <kunitoki@gmail.com>
2025-10-26 14:06:24 +0100

    More tweaks

--- src/etch/common/constants.nim
+++ src/etch/common/constants.nim
@@ -30,15 +30,20 @@ const
 
 
 # Function utils
+# Using :: and : as separators for readability (resembles C++/Rust namespace syntax):
+# - :: and : are NOT valid identifier characters in Etch (identifiers are [a-zA-Z_][a-zA-Z0-9_]*)
+# - : is only used for type annotations, not in function names
+# - This prevents conflicts with user functions like "my__method" which would break with "__" separator
+# - Makes mangled names readable: my__method::Is:i
 const
-  FUNCTION_NAME_SEPARATOR_STRING* = "__"
-  FUNCTION_RETURN_SEPARATOR_STRING* = "_"
+  FUNCTION_NAME_SEPARATOR_STRING* = "::"     # Separates function name from signature: funcName::signature
+  FUNCTION_RETURN_SEPARATOR_STRING* = ":"    # Separates parameters from return type: params:returnType
 
 
 # Bytecode serialization constants
 const
   BYTECODE_MAGIC* = "ETCH"
-  BYTECODE_VERSION* = 25  # Added varMaps for variable name display in debugging
+  BYTECODE_VERSION* = 26  # Changed function name separator from __ to $$ and _ to $
 
 
 # Symbolic execution constants
